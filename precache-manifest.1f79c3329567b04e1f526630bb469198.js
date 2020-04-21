795b23920cd0faf1959232d5ce032f4767d1b47d
edres-baadani <edres.baadani@outlook.com>
2020-04-21 20:57:10 +0300

    Updates

---
+++ precache-manifest.1f79c3329567b04e1f526630bb469198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06654c9e2432d665b6e758ec7fa7ed1",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "59705ad5ebaea746a459",
+    "url": "/react-tutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "607c18d14458d18821eb",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59705ad5ebaea746a459",
+    "url": "/react-tutorial/static/js/main.fbb7c47e.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
